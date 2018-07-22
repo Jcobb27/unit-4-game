92b754ad86664f776a7be4e55f53278fedfacee7
Jennifer Cobb <jenn.cobb.m@gmail.com>
2018-07-21 23:01:36 -0400

    initial layout and est. variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    var jewelsList = ["blue", "green", "pink", "purple"];
+    var targetScore;
+    var totalScore;
+    var wins;
+    var losses;
+
+    targetScore = Math.floor(Math.random() * 200) + 50;
+    $("#randomBox").text(targetScore);
+
+// function newGame() {
+//         targetScore = math.floor(math.random() * 200) + 50;
+//         $("#randomBox").text(targetScore);
+//     }
+
+
+});
